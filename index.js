b7a3e2867b95e27df5550071df07cd0b9e0599e9
Aishwary Gupta <aish042004@gmail.com>
2024-08-22 19:38:21 +0530

    Made index.html to see the live preview

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 import * as THREE from "three";
 
-const scene = new THREE.scene();
+const scene = new THREE.Scene();
+
+//cube 
+const geometry = new THREE.BoxGeometry(4, 1, 4);
+const material = new THREE.MeshLambertMaterial({ color: 0x00fD00 });
+const mesh = new THREE.Mesh(geometry, material);
+mesh.position.set(0, 0, 0);
+scene.add(mesh);
+
+//lights
+
+
