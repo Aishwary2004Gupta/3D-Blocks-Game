c84202f8e369c900a5ce7a5ddbf74b22b8ece027
Aishwary Gupta <aish042004@gmail.com>
2024-09-15 17:55:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -131,13 +131,13 @@ function addTransparentFloor() {
 
   // Create cloud puffs
   cloudGroup = new THREE.Group(); // Use the global cloudGroup variable
-  const numPuffs = 20;
+  const numPuffs = 60;
 
   for (let i = 0; i < numPuffs; i++) {
     const cloudPuff = new THREE.Mesh(cloudGeometry, cloudMaterial);
     
     const angle = (i / numPuffs) * Math.PI * 2;
-    const radius = floorSize / 3 * (Math.random() * 2);
+    const radius = floorSize / 3 * (Math.random() * 3);
     cloudPuff.position.set(
       Math.cos(angle) * radius,
       (Math.random() - 0.5) * floorHeight * 0.5,
